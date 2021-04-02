db317a0643574f2372c01eb3ebe422b694097ef1
Chris Minett <1084019+chrisminett@users.noreply.github.com>
2021-04-02 07:41:18 +0100

    Change test API credentials to environment vars
    This allows the credentials to be added in the CI config.

--- tests/LitmusTest.php
+++ tests/LitmusTest.php
@@ -40,11 +40,11 @@ class LitmusTest extends TestCase
 
     public function testGetSpamSeedAddresses(): void
     {
-        if (!$_SERVER['apiKey'] || !$_SERVER['apiPass']) {
+        if (!getenv('INTEGRATION_ENABLED')) {
             static::markTestSkipped('You must provide your own Litmus API credentials to test connection.');
         }
 
-        $litmusAPI = new Litmus($_SERVER['apiKey'], $_SERVER['apiPass']);
+        $litmusAPI = new Litmus(getenv('LITMUS_API_KEY'), getenv('LITMUS_API_PASS'));
         $spamSeedAddresses = $litmusAPI->getSpamSeedAddresses();
         static::assertIsArray($spamSeedAddresses);
         static::assertNotCount(0, $spamSeedAddresses);
@@ -52,11 +52,11 @@ class LitmusTest extends TestCase
 
     public function testGetEmailClients(): void
     {
-        if (!$_SERVER['apiKey'] || !$_SERVER['apiPass']) {
+        if (!getenv('INTEGRATION_ENABLED')) {
             static::markTestSkipped('You must provide your own Litmus API credentials to test connection.');
         }
 
-        $litmusAPI = new Litmus($_SERVER['apiKey'], $_SERVER['apiPass']);
+        $litmusAPI = new Litmus(getenv('LITMUS_API_KEY'), getenv('LITMUS_API_PASS'));
         $clients = $litmusAPI->getEmailClients();
         static::assertIsArray($clients);
     }
